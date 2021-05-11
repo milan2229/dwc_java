cda08a0138b87d35d639f08fbf49d77e0472dbcb
milan2229 <milan_1899.22@icloud.com>
2021-05-11 13:41:52 +0900

    first commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		String str = "成せばなる";
+		System.out.println(str);
+		
+		str = "人事尽くし天命を待つ";
+		System.out.println(str);
+		
+		System.out.println(MIN_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 100000000000L;
+		System.out.println(l);
+		double d = 1234567.89;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		
+		
+		Integer i = 123456789;
+		System.out.println(i);
+		Float f = 9.223372036854775807f;
+		System.out.println(f);
+		Character c = 'う';
+		System.out.println(c);
+		
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
